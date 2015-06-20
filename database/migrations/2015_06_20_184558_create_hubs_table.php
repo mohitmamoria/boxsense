bd02223a10510300b93b6efbbec1920ab58438a5
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-06-20 18:50:08 +0000

    Initial commit

---
+++ database/migrations/2015_06_20_184558_create_hubs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hubs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uuid');
+            $table->string('salt');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('hubs');
+    }
+}
