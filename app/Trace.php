56f976afc446d5310464425ea2190fbcff05e5b2
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-06-20 19:12:05 +0000

    created trace model

---
+++ app/Trace.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Trace extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'traces';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['hub_id', 'node_id', 'node_generation', 'type', 'value', 'created_at_node'];
+}
