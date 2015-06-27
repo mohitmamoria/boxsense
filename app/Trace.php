c173cbf09c3504c2811d5c0a8a411ef5567d5e2b
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-06-27 23:58:21 +0530

    added relationship in traces

--- app/Trace.php
+++ app/Trace.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Node;
 
 class Trace extends Model
 {
@@ -19,4 +20,9 @@ class Trace extends Model
      * @var array
      */
     protected $fillable = ['hub_id', 'node_id', 'value', 'created_at_node'];
+
+    public function node()
+    {
+        return $this->belongsTo(Node::class);
+    }
 }
