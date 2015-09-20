a0f90ebc0c472138c8509e64f84f052719d98b57
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-09-20 16:59:15 +0530

    added the use statements on Request class

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -3,13 +3,16 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Crypt;
+use RequestHash;
+use App\Hub;
 
 abstract class Request extends FormRequest
 {
     public function validHash()
     {
         if(config('auth.bypass_hash')) return true;
-        
+
         $hub = Hub::findByUuid($this->route()->parameter('hub_id'));
         $salt = Crypt::decrypt($hub->salt);
 
