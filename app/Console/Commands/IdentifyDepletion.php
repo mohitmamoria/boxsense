fdb1052bc5ff3102d860f127774a8b536a2fe280
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-10-24 14:46:41 +0530

    integrated horntell

--- app/Console/Commands/IdentifyDepletion.php
+++ app/Console/Commands/IdentifyDepletion.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Hub, App\Node, App\Trace;
+use Horntell;
 
 class IdentifyDepletion extends Command
 {
@@ -29,6 +30,8 @@ class IdentifyDepletion extends Command
     public function __construct()
     {
         parent::__construct();
+        Horntell\App::init('81vtnAxVtt8a7faFKc45n6dStBQapMhotXCfkYCm', 'Xa7KadbFBSWww9UYH9V9mhMsdxBGCCMphOdfxEjh');
+        Horntell\App::setBase('http://demo.api.horntell.com');
     }
 
     /**
@@ -44,10 +47,16 @@ class IdentifyDepletion extends Command
         $this->comment('Connected nodes found: '. $nodes->count().PHP_EOL);
         foreach($nodes as $node)
         {
-            $this->comment('Trying for Node: '.$node->uuid.' having latest trace: '.$node->latestTrace()->value.' against threshold: '. (int) $this->argument('threshold').PHP_EOL);
-            if($node->latestTrace()->value < (int) $this->argument('threshold'))
+            $latestTrace = $node->latestTrace()->value;
+            $this->comment('Trying for Node: '.$node->uuid.' having latest trace: '.$latestTrace.' against threshold: '. (int) $this->argument('threshold').PHP_EOL);
+            if($latestTrace < (int) $this->argument('threshold'))
             {
                 $this->comment('Notifying HUB: '.$node->hub->uuid.' for Node: '.$node->uuid.PHP_EOL);
+                (new Horntell\Campaign)->toProfile(
+                    $node->hub->uuid,
+                    '562b4ad39f17f6a06d8b4567',
+                    ['node_uuid' => $node->uuid, 'latest_trace' => $latestTrace]
+                );
             }
         }
         $this->info('Finishing at... '.time().PHP_EOL);
